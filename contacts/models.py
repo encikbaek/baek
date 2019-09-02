18f42870985f3fbe6d8b0427a82b6dc2d381cbd4
baek_portal <nazrin@baek.tech>
2019-09-03 07:05:21 +0800

    up

--- contacts/models.py
+++ contacts/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from datetime import datetime
 
 class Contact(models.Model):
-    client = models.CharField(max_length=200)
-    client_id = models.IntegerField()
+    #client = models.CharField(max_length=200)
+    #client_id = models.IntegerField()
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
-    user_id = models.IntegerField(blank=True)
+    #user_id = models.IntegerField(blank=True)
     def __str__(self):
         return self.name
